cf8f9a2ff5e4f6237aa2fd2c0b154356d5a7f59f
Mateo Harfuch <mharfuch@gmail.com>
2021-11-22 11:05:17 -0300

    fix: tests

--- tests/core/test_crudapi.py
+++ tests/core/test_crudapi.py
@@ -1,7 +1,25 @@
+from json import dump
+from json import load
+
 from crudapi import CrudAPI
+
+
+OPENAPI = "tests/core/openapi.json"
 
 
 def test_defaults(TestModel):
 
     app = CrudAPI()
     app.include_model(TestModel)
+
+
+def test_multiapp_openapi(client):
+    openapi = client.get("/openapi.json").json()
+    with open(OPENAPI) as file:
+        expected = load(file)
+    assert openapi == expected
+
+
+def dump_openapi(openapi):
+    with open(OPENAPI, "w") as file:
+        dump(openapi, file)
--- tests/core/test_multiapp.py
+++
@@ -1,8 +0,0 @@
-from json import load
-
-
-def test_multiapp_openapi(client):
-    openapi = client.get("/openapi.json").json()
-    with open("tests/core/openapi.json") as file:
-        expected = load(file)
-    assert openapi == expected
