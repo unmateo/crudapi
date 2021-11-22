4902070605699c39985e9ba2c51e31321273d50e
Mateo Harfuch <mharfuch@gmail.com>
2021-11-22 20:08:36 -0300

    feat(delete): db and service as dep

--- crudapi/mixins/delete.py
+++ crudapi/mixins/delete.py
@@ -1,16 +1,34 @@
 from ..routers import DeleteRouter
 from .base import router_extras
+from crudapi.core.dependencies import db as default_db
+from crudapi.services import DeleteService
+from crudapi.services import SearchService
 
 
 class DeleteMixin:
-    def delete_router(self, orm_model, response_model=None, **kwargs):
+    def delete_router(
+        self,
+        orm_model,
+        response_model=None,
+        db=default_db,
+        search_service=None,
+        delete_service=None,
+        **kwargs
+    ):
         """Include a default delete router.
 
         Override this method if custom behavior is required.
         """
         response_model = response_model or orm_model
-        extras = router_extras(orm_model, **kwargs)
+        search_service = SearchService(orm_model)
+        delete_service = DeleteService(orm_model)
         delete = DeleteRouter()
-        delete.map_routes(orm_model=orm_model, response_model=response_model)
+        delete.map_routes(
+            response_model=response_model,
+            db=db,
+            search_service=search_service,
+            delete_service=delete_service,
+        )
+        extras = router_extras(orm_model, **kwargs)
         self.include_router(delete, **extras)
         return delete
--- crudapi/routers/delete.py
+++ crudapi/routers/delete.py
@@ -1,30 +1,38 @@
 from fastapi import Depends
 from fastapi.routing import APIRouter
 
-from crudapi.core.dependencies import db
-from crudapi.services import DeleteService
-from crudapi.services import SearchService
-
 
 class DeleteRouter(APIRouter):
     """Augmented API Router with methods for generating default routes."""
 
-    def map_routes(self, orm_model, response_model):
-        self.delete_service = DeleteService(orm_model)
-        self.search_service = SearchService(orm_model)
-        self.map_delete(response_model)
+    def map_routes(
+        self,
+        response_model,
+        db,
+        search_service,
+        delete_service,
+    ):
+        self.map_delete(response_model, db, search_service, delete_service)
 
-    def map_delete(self, response_model):
+    def map_delete(self, response_model, db, search_service, delete_service):
         """ """
+        delete_endpoint = self._delete(
+            db=db,
+            search_service=search_service,
+            delete_service=delete_service,
+        )
         self.add_api_route(
             path="/{id}",
             methods={"DELETE"},
-            endpoint=self.delete,
+            endpoint=delete_endpoint,
             response_model=response_model,
             summary="Delete an instance.",
         )
 
-    def delete(self, id: str, db=Depends(db)):
-        """Delete an instance."""
-        instance = self.search_service.get_one(db, id)
-        return self.delete_service.delete(db, instance)
+    def _delete(self, db, search_service, delete_service):
+        def delete(id: str, session=Depends(db)):
+            """Delete an instance."""
+            instance = search_service.get_one(session, id)
+            return delete_service.delete(session, instance)
+
+        return delete
